dd9e186090812ee152ec4e37843f840d947400f0
alishaiaz <shaiazali@gmail.com>
2023-06-08 02:44:54 +0600

    update

--- script.py
+++ script.py
@@ -1,8 +1,6 @@
 import json
 import os
 
-# Specify the folder path containing the JSON files
-# source_folder = '/path/to/json/files/'
 source_folder = '/Users/shaiazali/Documents/Interviews/Quantigo AI/sampleJson/Files'
 destination_folder = '/Users/shaiazali/Documents/Interviews/Quantigo AI/sampleJson/Formatted_Files'
 
@@ -10,23 +8,36 @@ for filename in os.listdir(source_folder):
     if filename.endswith('.json'):
         with open(os.path.join(source_folder, filename), 'r') as json_file:
             json_data = json.load(json_file)
-            # print(json_data['objects'][0]['classTitle'])
 
-
-        annotation_objects = {}
-        annotation_attributes = {}
-        for objects in json_data['objects']:
-            # Presence Checker
-            presence_flag = 0
-            if (objects['classTitle'] == 'Vehicle' or objects['classTitle'] == 'License Plate'):
-                presence_flag = 1
-
-            # Defining Structure
-            object_name = objects['classTitle'].replace(' ', '_').lower()
-            annotation_objects[object_name] = {
-                "presence": presence_flag,
+        # Defining Structure #
+        annotation_objects = {
+            "vehicle": {
+                "presence": 0,
+                "bbox": []
+            },
+            "license_plate": {
+                "presence": 0,
                 "bbox": []
             }
+        }
+        annotation_attributes = {
+            "vehicle": {},
+            "license_plate": {
+                "Difficulty Score": 0,
+                "Value": "",
+                "Occlusion": 0
+            }
+        }
+
+        for objects in json_data['objects']:
+            object_name = objects['classTitle'].replace(' ', '_').lower()
+
+            # Presence Checker
+            presence_flag = 0
+            if object_name == 'vehicle':
+                annotation_objects['vehicle']['presence'] = 1
+            if object_name == 'license_plate':
+                annotation_objects['license_plate']['presence'] = 1
 
             # setting bbox
             for subList in objects['points']['exterior']:
@@ -35,18 +46,15 @@ for filename in os.listdir(source_folder):
             # Adding Attributes
             tempDict = {}
             for tags in objects['tags']:
-                tempDict[tags['name']] = tags['value']
-            annotation_attributes[object_name] = tempDict
-
-        # print(annotation_objects)
-        # print(annotation_attributes)
+                if (tags['name'] == "Difficulty Score"):
+                    tempDict[tags['name']] = int(tags['value'])
+                    # print(tempDict)
+                else:
+                    tempDict[tags['name']] = tags['value']
+            annotation_attributes[object_name].update(tempDict)
 
         # Convert the JSON data to the desired format
         formatted_json_data = [
-            # 'filename': filename.replace('.json', ''),
-            # 'data': json_data
-            # Add more fields or modify the existing fields as needed
-
             {
                 'dataset_name': filename,
                 'image_link': '',
@@ -57,10 +65,10 @@ for filename in os.listdir(source_folder):
         ]
 
         # Generate the new file name
-        new_filename = 'formatted_' + filename
+        new_json_filename = 'formatted_' + filename
 
-        # Write the formatted JSON data to a new file
-        with open(os.path.join(destination_folder, new_filename), 'w') as new_json_file:
-            json.dump(formatted_json_data, new_json_file, indent=4)
+        # Writing the formatted JSON data to a new file
+        with open(os.path.join(destination_folder, new_json_filename), 'w') as new_json_file:
+            json.dump(formatted_json_data, new_json_file, indent=2)
 
-        print(f"Formatted JSON saved as {new_filename}")
+        print(f"Formatted JSON saved as {new_json_filename}")
